653cf90870784273807e8d442ce5db57da56c1e9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-19 16:49:54 -0600

    First simple chat with Python sockets sucessfull integration.

--- SimpleChatSystemBackgroundWithSockets/client.py
+++ SimpleChatSystemBackgroundWithSockets/client.py
@@ -1,17 +1,27 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-client.connect(('localhost', 9999))
+client.connect(('localhost', 8600))
 
 done = False
 
 while not done:
-	client.send(input('Message: ').encode('utf-8'))
-	msg = client.recv(1024).decode('utf-8')
-	if msg == 'quit':
-		done = True
-	else:
-		print(msg)
-		
+    # Client waits for server's message first
+    msg = client.recv(1024).decode('utf-8')
+    print(f"Server: {msg}")
+
+    # If server wants to quit
+    if msg.lower() == "quit":
+        done = True
+        break
+
+    # Client sends response
+    msg = input("Client: ")  
+    client.send(msg.encode('utf-8'))
+
+    # If client wants to quit
+    if msg.lower() == "quit":
+        done = True
+        break
+
 client.close()
--- SimpleChatSystemBackgroundWithSockets/server.py
+++ SimpleChatSystemBackgroundWithSockets/server.py
@@ -1,22 +1,35 @@
 import socket
 
-# Server socket bint to "localhost" url in port 9999 using AF_INET and SOCK_STREAM
+# Server socket definition
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999)) # use "socket.gethostname()" to make it vissible worldwide
+server.bind(('localhost', 8600)) # use "socket.gethostname()" to make it vissible worldwide
+server.listen(5) # Server with 5 connection request. Make this socker to be a server
 
-server.listen(5) # Set the Server socket to hear just 5 connection request, let in blank if infinite requests are allowed (not recommended), with this function it becomes a server socket, without it it stills being a simple client socket
+print("Server´s waiting for connection...")
 
-client, addr = server.accept() # Accepting the client to connect to this server in the same channel
+client, addr = server.accept() # Accepting the client to connect to server
+
+print(f"Server´s connected to client: {addr}")
 
 done = False
 
 while not done:
-    msg = client.recv(1024).decode('utf-8') # Message is what the client socket will sent in 1024 bytes in utf-8 decoded format
-    if msg == 'quit':
+    # Server sends first
+    msg = input("Server: ")
+    client.send(msg.encode('utf-8')) 
+
+    if msg.lower() == 'quit':
         done = True
-    else:
-        print(msg)
-    client.send(input('Message': ).encode('utf-8')) # Messages obtained from what the client sends as message decoded in utf-8
+        break
+    
+    # Server waits for client response
+    msg = client.recv(1024).decode('utf-8')
+    print(f"Client: {msg}")
+    
+    # If client wants to quit
+    if msg.lower() == "quit":
+        done = True
+        break
 
 client.close()
 server.close()
