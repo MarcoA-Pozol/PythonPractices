64aa27dbe52da972e688bd192016b3249c758236
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-19 15:48:58 -0600

    Minor changes to sockets simple system

--- SimpleChatSystemBackgroundWithSockets/server.py
+++ SimpleChatSystemBackgroundWithSockets/server.py
@@ -2,16 +2,16 @@ import socket
 
 # Server socket bint to "localhost" url in port 9999 using AF_INET and SOCK_STREAM
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
+server.bind(("localhost", 9999)) # use "socket.gethostname()" to make it vissible worldwide
 
-server.listen() # Set the Server socket to hear
+server.listen(5) # Set the Server socket to hear just 5 connection request, let in blank if infinite requests are allowed (not recommended), with this function it becomes a server socket, without it it stills being a simple client socket
 
 client, addr = server.accept() # Accepting the client to connect to this server in the same channel
 
 done = False
 
 while not done:
-    msg = client.recv(1024).decode('utf-8') # Message is what the client socket will sent from port 1024 in utf-8 decoded format
+    msg = client.recv(1024).decode('utf-8') # Message is what the client socket will sent in 1024 bytes in utf-8 decoded format
     if msg == 'quit':
         done = True
     else:
