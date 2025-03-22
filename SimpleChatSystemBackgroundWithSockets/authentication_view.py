2a6d385b48436e2cbd7c21c2827eadc8224083bf
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-21 18:52:10 -0600

    Cursor class for most common queries management definition and inclution of the first view for Authentication formulary using tkinter --in progress

--- SimpleChatSystemBackgroundWithSockets/authentication_view.py
+++ SimpleChatSystemBackgroundWithSockets/authentication_view.py
@@ -1,18 +1,50 @@
 import tkinter as tk
-from . database import database_object
+from . database import database_object, Cursor
 
 # Queries
 create_table_query = """CREATE TABLE users IF NOT EXITS (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)"""
 insert_new_user_query = """INSERT INTO users (username, password) VALUES (?,?)""".format(username, password)
 
-database_object.execute_query(query=create_table_query)
+
+database_object = Database(database_name='ChatDatabase.db')
 query_insert = """INSERT INTO users"""
 
 
-def validate_user(username:str, password:str) -> bool:
+def validate_user(username:str, password:str, cursor:Cursor) -> bool:
     """
         Validate user's provided data to check if user exists or not.
     """
+    cursor = cursor
+    
+    # Behavior
+    cursor.execute_select_query()
     
     user = None
     return user not None
+   
+   
+   
+root = tk.Tk()
+root.title("Chat With Sockets | MarcoA-Pozol")
+root.size("1200x600+0+0")
+
+register_formulary = tk.Label(root, bg='blue', fg='white')
+register_formulary.position(x=50, y=50)
+
+# username field
+username_label = tk.Label(register_formulary, fg='white')
+username_label.grid(row=0, column=0)
+
+username_entry = tk.Entry(register_formulary, fg='white', bg='green')
+username_entry.grid(row=0, column=1)
+
+
+# password field
+password_label = tk.Label(register_formulary, fg='white')
+password_label.grid(row=1, column=0)
+
+password_entry = tk.Entry(register_formulary, fg='white', bg='green')
+password_entry.grid(row=1, column=1)
+
+button_register = tk.Button(register_formulary, text="Create", command=lambda:validate_user(username=username_entry.get(), password=password_entry.get()))
+root.mainloop()
--- SimpleChatSystemBackgroundWithSockets/database.py
+++ SimpleChatSystemBackgroundWithSockets/database.py
@@ -6,6 +6,11 @@ class AbstractDatabase(ABC):
     @execute_query
     def execute_query(self) -> None:
         pass
+       
+class AbstractCursor(ABC):
+    @abstractmethod
+    def execute_select_query(table_name:str, fields:list[str]) -> list:
+        pass
 
 class Database(AbstractDatabase):
 	def ___init__(self, database_name:str) -> None:
@@ -17,5 +22,29 @@ class Database(AbstractDatabase):
 			Executes a query of any type.
 		"""
 		self.cursor.execute(query)
-		
-database_object = Database(database_name='ChatDatabase.db')
+        
+class Cursor(AbstractCursor):
+    def __init__(self, database_object:Database):
+        self.__database = database_object
+        self.__cursor = self.__database.cursor()
+    
+    # Props setter and getter methods
+    @property
+    def database(self) -> Database:
+        return self.__database
+    
+    @property
+    def cursor(self):
+        return self.__cursor
+    
+    # Methods
+    def execute_select_query(self, table_name:str, fields:list[str]) -> list:
+        """
+            Receives a table name and a group of fields to execute a simple SELECT query to an specified database table and returns the queryset in list format.
+        """
+        try:
+            data = self.cursor.execute(f"""SELECT {field for field in fields}.join(', ') FROM {table_name};""").fetchone()
+            return data
+        except Exception as e:
+            print(e)
+            raise e
