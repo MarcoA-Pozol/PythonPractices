0592dffec0884fc442778ed249c7bf408e2553e3
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-24 20:20:47 -0600

    NotificationsSystem: Changes of structure of functions and workflow for server.py and gui.py files

--- SimpleChatSystemBackgroundWithSockets/NotificationsSystem/gui.py
+++ SimpleChatSystemBackgroundWithSockets/NotificationsSystem/gui.py
@@ -1,1 +1,38 @@
-from . import client
+from . client import receive_message
+import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from datetime import datetime
+
+# GUI for the Notifications System.
+root = tk.Tk()
+
+root.title("Notifications System")
+root.geometry("400x500+100+50")
+
+notifications_list = scrolledtext.ScrolledText(root, state=tk.DISABLED, wrap=tk.WORD).pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+# GUI Functions
+def receive_notifications():
+    """
+        Receive notifications from the client.
+
+        Attributes:
+        - None
+
+        Returns:
+        - None
+    """
+    notification = receive_message()
+    if notification:
+        notifications_list.config(state=tk.NORMAL)
+        
+        notifications_list.insert(tk.END, f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}  ---->  {notification}")
+
+        notifications_list.config(state=tk.DISABLED)
+        notification.yview(tk.END)
+
+# Start receiving notifications.
+threading.Thread(target=receive_notifications, daemon=True).start()
+
+root.mainloop()
--- SimpleChatSystemBackgroundWithSockets/NotificationsSystem/server.py
+++ SimpleChatSystemBackgroundWithSockets/NotificationsSystem/server.py
@@ -1,6 +1,7 @@
 import socket 
 import threading
 import logging
+from . notifications_client import notifications_client
 
 # Logging configuration.
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -11,9 +12,9 @@ server.bind(('localhost', 8500))
 server.listen(6) # This server accepts 6 client connections.
 
 # List of connected clients.
-clients = []
+clients = [notifications_client]
 
-def handle_client(client, addr):
+def handle_clients(client, addr):
     """
         Handle client's messages and connection.
 
@@ -28,8 +29,8 @@ def handle_client(client, addr):
 
     while True:
         try:
-            # Receive and decode message from the client.
-            message = client.recv(1024).decode('utf-8')
+            # Receive and decode messages from notifications client.
+            message = clients[0].recv(1024).decode('utf-8')
 
             # Close the server if client sent a '\q' request for server to close.
             if message == '\q':
@@ -38,7 +39,7 @@ def handle_client(client, addr):
             logging.info(message)
 
             # Send message to Notification's client.
-            clients[0].send(message.encode('utf-8'))
+            clients[1].send(message.encode('utf-8'))
 
         except ConnectionResetError as ce:
             logging.error(msg=f'Impossible to handle the client message: {ce}')
@@ -61,4 +62,4 @@ async def check_for_server_connections_availability(clients:list=clients) -> Non
     while len(clients) < 6:
         client, addr = server.accept()
         clients.append(client)
-        threading.Thread(target=handle_client, args=(client, addr)).start()
+        threading.Thread(target=handle_clients, args=(client, addr)).start()
